0464c50ba6bcfc0eeb0bf6e5d80173f1c31df797
Jarno Bakker <jarno.bakker@hotmail.nl>
2024-11-26 15:57:06 +0000

    add confetti

---
+++ portfolio-website/components/confetti.js
@@ -0,0 +1,67 @@
+import confetti from "canvas-confetti";
+
+export function dateEqualsDateAndMonth(date, dateToCompare) {
+  return (
+    date.getDate() === dateToCompare.getDate() &&
+    date.getMonth() === dateToCompare.getMonth()
+  );
+}
+
+export function confettiExplosion() {
+  const anniversaryDate = new Date("2023-12-09");
+  const birthdayKim = new Date("2004-12-02");
+  const birthdayJarno = new Date("2001-06-26");
+  const currentDate = new Date();
+
+  if (
+    dateEqualsDateAndMonth(currentDate, anniversaryDate) ||
+    dateEqualsDateAndMonth(currentDate, birthdayKim) ||
+    dateEqualsDateAndMonth(currentDate, birthdayJarno)
+  ) {
+    confetti({
+      particleCount: 80,
+      spread: 120,
+      origin: { y: 0.6 },
+    });
+  }
+}
+
+export function confettiAnimation() {
+  const currentDate = new Date();
+  const anniversaryDate = new Date("2023-12-09");
+
+  if (!dateEqualsDateAndMonth(currentDate, anniversaryDate)) {
+    return;
+  }
+  const duration = 8 * 1000,
+    animationEnd = Date.now() + duration,
+    defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  const interval = setInterval(function () {
+    const timeLeft = animationEnd - Date.now();
+
+    if (timeLeft <= 0) {
+      return clearInterval(interval);
+    }
+
+    const particleCount = 50 * (timeLeft / duration);
+
+    // since particles fall down, start a bit higher than random
+    confetti(
+      Object.assign({}, defaults, {
+        particleCount,
+        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+      }),
+    );
+    confetti(
+      Object.assign({}, defaults, {
+        particleCount,
+        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+      }),
+    );
+  }, 250);
+}
--- portfolio-website/pages/love-you.js
+++ portfolio-website/pages/love-you.js
@@ -2,9 +2,11 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import React from "react";
 import Clock from "../components/clock";
+import { confettiAnimation, confettiExplosion } from "../components/confetti";
 
 export default function LoveYou() {
-    
+  confettiAnimation(); //Only when its the anniversary date
+
   return (
     <div className={styles.container} style={{background: "#fddfe4"}}>
       <Head>
@@ -18,6 +20,7 @@ export default function LoveYou() {
             <img
                 className="w-24 md:w-72 lg:w-96 transform transition duration-1000 hover:scale-125"
                 src="heart.png"
+                onClick={confettiExplosion}
             />
         </div>
         <Clock/>
